494f3d3f05e2dbc7816a7cc3c25643ec1cf77269
Sodbrennen <Maximilian.Kosel@hhu.de>
2015-11-03 22:22:48 +0100

    Update fibonacci.cxx

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,1 +1,22 @@
 
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int N;
+	cin >> N;
+
+	int zn = 1;
+	int za = 1;
+	
+	
+	for(int i=1;i<=N-2;i++){
+		zn = zn + za;
+		za = zn - za;
+		}
+
+	cout << zn << endl;
+
+}
